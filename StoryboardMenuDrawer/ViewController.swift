c68f3ceebb962d200cb46c4326664eb6959bec63
Oge Nwabuoku <on475k@att.com>
2017-08-25 03:45:41 -0400

    First Commit alongside Storyboard Menu Drawer implementation

--- StoryboardMenuDrawer/ViewController.swift
+++ StoryboardMenuDrawer/ViewController.swift
@@ -9,15 +9,62 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var drawerButton: UIButton!
+    @IBOutlet weak var leftMenuContainer: UIView!
+    @IBOutlet weak var rightMenuContainer: UIView!
+    
+    @IBOutlet weak var mainViewTrailing: NSLayoutConstraint!
+    @IBOutlet weak var mainViewLeading: NSLayoutConstraint!
+    @IBOutlet var tapGestureRecognizer: UITapGestureRecognizer!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //Use Constraints and Width to control DrawerMenu
+    func toggleMenu(leftMenuDrawer: Bool){
+        
+        if leftMenuDrawer {
+        let closed = mainViewLeading.constant == 0
+        mainViewLeading.constant =  closed ? leftMenuContainer.frame.size.width : 0
+        mainViewTrailing.constant = closed ? -leftMenuContainer.frame.size.width : 0
+        } else {
+            let closed = mainViewLeading.constant == 0
+            mainViewLeading.constant =  closed ? -rightMenuContainer.frame.size.width : 0
+            mainViewTrailing.constant = closed ? rightMenuContainer.frame.size.width : 0
+        }
+        
+        UIView.animate(withDuration: 0.3) {
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    //Note: Adjust width of left OR right menu container
+    //in Storyboard to manipulate toggle to background view ratio.
+    @IBAction func drawerMenuButton(sender: UIButton) {
+        if sender.tag == 100 {
+            //Toggle left
+            toggleMenu(leftMenuDrawer: true)
+            rightMenuContainer.isHidden = true
+            leftMenuContainer.isHidden = false
+        } else {
+            //Toggle Right
+            toggleMenu(leftMenuDrawer: false)
+            rightMenuContainer.isHidden = false
+            leftMenuContainer.isHidden = true
+        }
+       
+    }
+    
+    //Close drawerMenu if Open
+    @IBAction func tapAction(sender: UITapGestureRecognizer) {
+        let closed = mainViewLeading.constant == 0
+        if (!closed){toggleMenu(leftMenuDrawer: true)}
+        
     }
 
 
